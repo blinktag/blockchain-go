9048c6be59810bcca7d8618f8aae183d53999502
Mark Gregan <mark@gregan.org>
2017-10-11 21:53:44 -0400

    Fixing comments in main

--- main.go
+++ main.go
@@ -10,10 +10,13 @@ func main() {
 	// Create root block
 	b := block.NewBlock(100, "")
 
-	// P
+	// Add sample transaction
 	b.NewTransaction("me@myself.com", "john@doe.com", 99)
+
+	// Mine block
 	b.Mine()
 
+	// Print out our current block chain
 	for _, block := range block.BlockChain {
 		fmt.Printf("%#v\n\n", block)
 	}
